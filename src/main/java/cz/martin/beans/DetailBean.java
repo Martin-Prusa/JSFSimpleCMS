71053e652fb3b6a880e18ddf39af4e222bcbdcbb
Martin Průša <martinprusa@Martin-iMac-24.localdomain>
2022-11-04 16:49:40 +0100

    detail

--- src/main/java/cz/martin/beans/DetailBean.java
+++ src/main/java/cz/martin/beans/DetailBean.java
@@ -7,6 +7,9 @@ import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.util.Map;
 
@@ -25,6 +28,13 @@ public class DetailBean {
         FacesContext fc = FacesContext.getCurrentInstance();
         Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
         id = params.get("id");
+    }
+
+    public String getFormatted(String md) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(md);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
     }
 
     public Post getPost() {
--- src/main/java/cz/martin/beans/MainBean.java
+++ src/main/java/cz/martin/beans/MainBean.java
@@ -6,25 +6,16 @@ import cz.martin.qualifiers.Normal;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
-import org.commonmark.node.Node;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
 
 import java.util.List;
 
-@Named("main")
+@Named("list")
 @ApplicationScoped
 public class MainBean {
     @Inject
     @Normal
     private IPostsService postsService;
 
-    public String getFormatted(String md) {
-        Parser parser = Parser.builder().build();
-        Node document = parser.parse(md);
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
-        return renderer.render(document);
-    }
     public List<Post> getPosts() {
         return this.postsService.getVisiblePosts();
     }
