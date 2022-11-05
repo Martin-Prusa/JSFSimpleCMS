105f8e5bb2f036427d481963971abd658c2d7e9f
Martin Průša <martinprusa@Martin-iMac-24.localdomain>
2022-11-05 11:29:21 +0100

    register functionality

--- src/main/java/cz/martin/beans/RegisterBean.java
+++ src/main/java/cz/martin/beans/RegisterBean.java
@@ -3,9 +3,11 @@ package cz.martin.beans;
 import cz.martin.interfaces.services.IActiveUserService;
 import cz.martin.qualifiers.Normal;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 @Named("register")
@@ -21,7 +23,38 @@ public class RegisterBean implements Serializable {
     private String passwordCheck;
     private String error;
 
-    public void register() {
+    public void register() throws IOException {
+        error = "";
+        if (password.equals(passwordCheck) && activeUserService.register(username, password))
+            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+        else error = "Passwords do not match or username is used";
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPasswordCheck(String passwordCheck) {
+        this.passwordCheck = passwordCheck;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPasswordCheck() {
+        return passwordCheck;
+    }
+
+    public String getError() {
+        return error;
     }
 }
